721ca6c0ddd911283258bddc14652203bf22028b
Artem Baikov <artem.baikov.darkmoon@gmail.com>
2024-03-31 13:37:20 +0300

    Initial commit

---
+++ lib/ecto_query_filter.ex
@@ -0,0 +1,18 @@
+defmodule EctoQueryFilter do
+  @moduledoc """
+  Documentation for `EctoQueryFilter`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoQueryFilter.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ecto_query_filter_test.exs
@@ -0,0 +1,8 @@
+defmodule EctoQueryFilterTest do
+  use ExUnit.Case
+  doctest EctoQueryFilter
+
+  test "greets the world" do
+    assert EctoQueryFilter.hello() == :world
+  end
+end
